82c27beb92d64534d1f8e76573fe67d67cb97a57
Sandro Fernandes <68675374+SandroFernandesRosal@users.noreply.github.com>
2022-06-30 17:04:26 -0300

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,16 +1,18 @@
-var botao = document.querySelector('.botao')
-var menu = document.querySelector('.botaomenu')
+var botao = document.querySelector('.botao');
+var menu = document.querySelector('.botaomenu');
+var icon = document.querySelector('.icon');
 
 botao.addEventListener("click", menumobile);
 function menumobile(){
 
   if (menu.style.display =='none'){
     menu.style.display='block';
-
+    icon.src="./IMG/close.png";
   }
  
   else {
     menu.style.display='none';
+    icon.src="./IMG/botao-menu.png";
   }
 }
 
